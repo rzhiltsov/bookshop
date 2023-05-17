f0f0b7ed32643f712344e9adda25dc7f5fe24f90
Роман Жильцов <zhiltsov.ri@gmail.com>
2023-05-17 11:26:29 +0300

    Set popular page

--- Bookshop/src/main/java/com/example/MyBookShopApp/controllers/PopularPageController.java
+++ Bookshop/src/main/java/com/example/MyBookShopApp/controllers/PopularPageController.java
@@ -1,13 +1,24 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PopularPageController {
 
+    private final BookService bookService;
+
+    @Autowired
+    public PopularPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/books/popularPage")
-    public String popularPage() {
+    public String popularPage(Model model) {
+        model.addAttribute("popularBooks", bookService.getPopularBooks(0, 20));
         return "/books/popular";
     }
 }
